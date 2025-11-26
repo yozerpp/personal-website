76b9abfb27ca2ac7c236cd808c101a75f011f8fc
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-11-27 00:26:33 +0300

    make snippet's type param any

--- src/lib/components/ui/types.d.ts
+++ src/lib/components/ui/types.d.ts
@@ -1,4 +1,5 @@
 import TransitionConfig from 'svelte/transition'
+import {Snippet} from "svelte";
 
 export type TransitionFunction =
     ((node : Element, params? : any | undefined, options? : {direction: 'in' |'out'|'both'})=>TransitionConfig)
@@ -7,8 +8,8 @@ export type Color = string | number;
 export type ColorWithOpacity = {color: Color, opacity?: number}
 export type GradientColor = {direction: 't'| 'tr' | 'r' | 'br'| 'b'| 'bl' | 'l' |  'tl',from: Color | ColorWithOpacity, via?: Color | ColorWithOpacity, to: Color | ColorWithOpacity}
 export type SnippetHolder = {
-    args: Record<string, unknown>;
-    snippet: import("svelte").Snippet<[Record<string, unknown>]>;
+    args?: Record<string, unknown>;
+    snippet: Snippet<[any]>;
 };
 export type CssProp=string | {
     normal: string,
