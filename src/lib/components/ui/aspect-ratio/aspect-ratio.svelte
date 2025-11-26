08a2cddf2597353df179161cac05bbc1613d3522
yozerpp <tipili-yusuf-ozer@hotmail.com>
2025-11-27 00:33:57 +0300

    add classNames to aspect-ratio

--- src/lib/components/ui/aspect-ratio/aspect-ratio.svelte
+++ src/lib/components/ui/aspect-ratio/aspect-ratio.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { AspectRatio as AspectRatioPrimitive } from "bits-ui";
 
-	let { ref = $bindable(null), ...restProps } = $props();
+	let { ref = $bindable(null), class: classNames, ...restProps } = $props();
 </script>
 
-<AspectRatioPrimitive.Root bind:ref data-slot="aspect-ratio" {...restProps} />
+<AspectRatioPrimitive.Root bind:ref data-slot="aspect-ratio" class={classNames} {...restProps} />
